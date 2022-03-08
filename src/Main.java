b849b757c8afbfa4ad2785a58c3f09168f3fce12
Daniel Forti <daforti@unibz.it>
2022-03-08 16:53:23 +0100

    added two  new .java files

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog snuffles = new Dog("Snuffles", "Maltese", 20, 5.5);
+        System.out.println(snuffles);
+
+        Dog marley = new Dog("Marley", "Labrador", 55, 29);
+        System.out.println(marley);
+    }
+}
